31a224041567b8309d7dc8e1b8cb62436a3ac2e3
Sharan-Python <75255241+Sharan-Python@users.noreply.github.com>
2020-12-01 07:25:16 +0530

    Update MusicPlayer.py

--- Music player/MusicPlayer.py
+++ Music player/MusicPlayer.py
@@ -2,7 +2,7 @@ from tkinter import *
 from tkinter import ttk
 import os
 import pygame
-from tinytag import TinyTag, TinyTagException
+from tinytag import TinyTag
 
 temp = TinyTag.get("Unforgettable lyrics - clean.mp3")
 
@@ -19,21 +19,24 @@ q = PhotoImage(file="PlayButton.png")
 t = PhotoImage(file="PauseButton.png")
 o = PhotoImage(file="Forward.png")
 r = PhotoImage(file="Rewind.png")
+h = PhotoImage(file="loop.png")
+
 
 def buttonpress(n):
-    if n==1:
-        Button(win, image=t, borderwidth=0, command=lambda: buttonpress(2)).place(x=575, y=700)# Pause button
+    if n == 1:
+        Button(win, image=t, borderwidth=0, command=lambda: buttonpress(2)).place(x=575, y=700)  # Pause button
         # play/pause with pyaudio will come here
         pygame.mixer.init()
-        pygame.mixer.music.load('Unforgettable lyrics - clean.mp3')
+        pygame.mixer.music.load('Google.wav')
         pygame.mixer.music.play(loops=0)
 
-    if n==2:
+    if n == 2:
         Button(win, image=q, borderwidth=0, command=lambda: buttonpress(3)).place(x=575, y=700)
         pygame.mixer.music.pause()
-    if n==3:
+    if n == 3:
         Button(win, image=t, borderwidth=0, command=lambda: buttonpress(2)).place(x=575, y=700)
         pygame.mixer.music.unpause()
+
 
 def set_vol(val):
     volume = int(val) / 100
@@ -41,16 +44,14 @@ def set_vol(val):
     # set_volume of mixer takes value only from 0 to 1. Example - 0, 0.1,0.55,0.54.0.99,1
 
 
-scale = Scale( from_=100, to=0, orient=VERTICAL, command=set_vol)
+scale = Scale(from_=0, to=100, orient=HORIZONTAL, command=set_vol)
 scale.set(50)
 pygame.mixer.music.set_volume(0.7)
-scale.place(x=725,y=655)
+scale.place(x=1145, y=860)
 
-
-Button(win, image=q, borderwidth=0, command=lambda: buttonpress(1)).place(x=575, y=700)# Play button
-Button(win, image=o, borderwidth=0).place(x=634, y=700)
-Button(win, image=r, borderwidth=0, command=lambda: buttonpress(1)).place(x=516, y=700)
+Button(win, image=q, borderwidth=0, command=lambda: buttonpress(1)).place(x=1015, y=840)  # Play button
+Button(win, image=o, borderwidth=0).place(x=1075, y=840)
+Button(win, image=r, borderwidth=0, command=lambda: buttonpress(1)).place(x=955, y=840)
 
 
 win.mainloop()
-#random
