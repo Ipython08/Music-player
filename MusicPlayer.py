866a613bd6f78adfdd7e2457606167778b6b9fc6
Ipython08 <64698562+Ipython08@users.noreply.github.com>
2020-12-01 17:49:24 +0530

    Update MusicPlayer.py
    added colour modes

--- MusicPlayer.py
+++ MusicPlayer.py
@@ -7,8 +7,7 @@ import tkinter
 import pygame
 from tinytag import TinyTag, TinyTagException
 
-
-a = 0
+a=0
 
 win = Tk()
 win.title("Music Player")
@@ -22,49 +21,56 @@ pygame.mixer.init()
 menubar = Menu(win)
 win.config(menu=menubar)
 
+def light():
+    win["bg"] = "white"
+
+def dark():
+    win["bg"] = "#696969"
 
 # Create the submenu
 
 def about():
-    tkinter.messagebox.showinfo("About us",
-                                "This is an unnamed music player that is still in development made by Ishanth Rajesh and Sharan Senthil")
-
-
+    tkinter.messagebox.showinfo("About us","This is an unnamed music player that is still in development made by Ishanth Rajesh and Sharan Senthil")
 def openfile():
     global file
     file = filedialog.askopenfilename()
-
-
+    
 subMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="File", menu=subMenu)
-subMenu.add_command(label="Open", command=openfile)
-subMenu.add_command(label="Exit", command=win.destroy)
+subMenu.add_command(label="Open",command=openfile)
+subMenu.add_command(label="Exit",command=win.destroy)
+
+subMenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Modes", menu=subMenu)
+subMenu.add_command(label="Light",command=light)
+subMenu.add_command(label="Dark",command=dark)
+
 
 subMenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Help", menu=subMenu)
-subMenu.add_command(label="About Us", command=about)
+subMenu.add_command(label="About Us",command=about)
 
 q = PhotoImage(file="PlayButton.png")
 t = PhotoImage(file="PauseButton.png")
 o = PhotoImage(file="Forward.png")
 r = PhotoImage(file="Rewind.png")
 
-
 def buttonpress(n):
-    if n == 1:
-        Button(win, image=t, borderwidth=0, command=lambda: buttonpress(2)).place(x=1015, y=820)  # Pause button
+    
+    if n==1:
+        Button(win, image=t, borderwidth=0, command=lambda: buttonpress(2)).place(x=575, y=700)# Pause button
         # play/pause with pyaudio will come here
         pygame.mixer.init()
         pygame.mixer.music.load(file)
         pygame.mixer.music.play(loops=0)
 
-    if n == 2:
-        Button(win, image=q, borderwidth=0, command=lambda: buttonpress(3)).place(x=1015, y=820)
+    if n==2:
+                    
+        Button(win, image=q, borderwidth=0, command=lambda: buttonpress(3)).place(x=575, y=700)
         pygame.mixer.music.pause()
-    if n == 3:
-        Button(win, image=t, borderwidth=0, command=lambda: buttonpress(2)).place(x=1015, y=820)
+    if n==3:
+        Button(win, image=t, borderwidth=0, command=lambda: buttonpress(2)).place(x=575, y=700)
         pygame.mixer.music.unpause()
-
 
 def set_vol(val):
     volume = int(val) / 100
@@ -72,13 +78,15 @@ def set_vol(val):
     # set_volume of mixer takes value only from 0 to 1. Example - 0, 0.1,0.55,0.54.0.99,1
 
 
-scale = Scale(from_=100, to=0, orient=VERTICAL, command=set_vol)
+scale = Scale( from_=100, to=0, orient=VERTICAL, command=set_vol)
 scale.set(50)
 pygame.mixer.music.set_volume(0.7)
-scale.place(x=725, y=655)
+scale.place(x=725,y=655)
 
-Button(win, image=q, borderwidth=0, command=lambda: buttonpress(1)).place(x=1015, y=820)  # Play button
-Button(win, image=o, borderwidth=0).place(x=1075, y=820)
-Button(win, image=r, borderwidth=0, command=lambda: buttonpress(1)).place(x=955, y=820) # what is the problem with these values
+
+Button(win, image=q, borderwidth=0, command=lambda: buttonpress(1)).place(x=575, y=700)# Play button
+Button(win, image=o, borderwidth=0).place(x=634, y=700)
+Button(win, image=r, borderwidth=0, command=lambda: buttonpress(1)).place(x=516, y=700)
+
 
 win.mainloop()
